e7175b25d3146b176fc7d7f2bdc53ffdfe38d738
qianlizhixing12 <qianlizhixing_12@163.com>
2021-10-31 21:54:53 +0800

    上传下载

--- blue/up_down.py
+++ blue/up_down.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify, request, make_response, render_template, send_from_directory
+import os
+
+from flask import Blueprint, jsonify, request, make_response, render_template, send_from_directory, url_for, redirect
+from werkzeug.utils import secure_filename
 
 blue_up_down = Blueprint("blue_up_down", __name__)
 
@@ -8,6 +11,18 @@ def up_down():
     return make_response(render_template("up_down.html"))
 
 
+img_path = os.path.join(os.path.abspath(os.curdir), "img")
+
+
 @blue_up_down.route(rule="img_download.do", methods=["POST"])
 def img_download():
-    return send_from_directory("img", "favicon.ico", as_attachment=True)
+    filename = request.form["filename"]
+    return send_from_directory(img_path, filename, as_attachment=True)
+
+
+@blue_up_down.route(rule="img_upload.do", methods=["POST"])
+def img_upload():
+    f = request.files['filename']
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(img_path, filename))
+    return redirect(url_for('blue_up_down.up_down'))
